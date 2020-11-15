f647ab4dcd048d060b4ba9a3e3458df3df7a5ed1
“HarishTan” <harishtanguturi18@gmail.com>
2020-11-15 15:12:53 -0600

    First commit
    Uploading all files

---
+++ harish_basic_java/src/main/java/com/example/demo/HarishBasicJavaApplication.java
@@ -0,0 +1,15 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HarishBasicJavaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HarishBasicJavaApplication.class, args);
+		
+		System.out.println();
+	}
+
+}
